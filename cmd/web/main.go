4f43b55119be3dc3250031dc2be0d928796477cd
xiuxi <xiuxi554325746@gmail.com>
2021-10-04 03:18:47 +0800

    a

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/xiuxi/go-course/pkg/handler"
+	"github.com/xiuxi/go-course/pkg/handlers"
 )
 
 const portNum = ":8080"
 
 func main() {
-	http.HandleFunc("/", handler.Home)
-	http.HandleFunc("/about", handler.About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 	fmt.Println(fmt.Sprintf("%s", portNum))
 	http.ListenAndServe(portNum, nil)
 }
